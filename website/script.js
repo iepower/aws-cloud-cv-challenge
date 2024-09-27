30a5de902f345af8a49cc3a257c13419239b2a5d
Power <kevehazi.beni2000@gmail.com>
2024-09-27 14:03:01 +0200

    Fix on js to display view count hopefully now in a right method

--- website/script.js
+++ website/script.js
@@ -1,24 +1,10 @@
-// visitorCounter.js
-
-// Function to update and display the visitor count
-const counter = document.querySelector('.counter-number');
+// JavaScript Code
+const counter = document.querySelector(".counter-number");
 
 async function updateCounter() {
-    try {
-        let response = await fetch("https://dh5c5iyzbkfh536wtkzz3jppam0prwnp.lambda-url.eu-north-1.on.aws/");
-        
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        let data = await response.json();
-
-        // Access the views property
-        counter.innerHTML = `Views: ${data.views}`;
-    } catch (error) {
-        console.error('Error fetching views:', error);
-        counter.innerHTML = "Couldn't read views";
-    }
+    let response = await fetch("https://dh5c5iyzbkfh536wtkzz3jppam0prwnp.lambda-url.eu-north-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = `Views: ${data}`;
 }
 
 updateCounter();
