ab413a24c4af2cc3e3312822cdd340cca48ff216
iepower <kevehazi.beni2000@gmail.com>
2024-09-28 19:55:32 +0200

    New lambda function url for the IaC created aws lambda

--- website/script.js
+++ website/script.js
@@ -2,7 +2,7 @@
 const counter = document.querySelector(".counter-number");
 
 async function updateCounter() {
-    let response = await fetch("https://dh5c5iyzbkfh536wtkzz3jppam0prwnp.lambda-url.eu-north-1.on.aws/");
+    let response = await fetch("https://b46pmugjigy47oiaxqguk7u2hu0somof.lambda-url.eu-north-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = `Views: ${data}`;
 }
